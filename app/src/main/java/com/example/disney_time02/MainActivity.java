b4217ea2797847afd59159a01a8df319b502e27b
LiliaChe <liliach574@gmail.com>
2022-06-07 12:25:27 +0300

    הוספת עמודים התחלתיים

--- app/src/main/java/com/example/disney_time02/MainActivity.java
+++ app/src/main/java/com/example/disney_time02/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.disney_time02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLog, btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.btnLog = findViewById(R.id.btnLog);
+        this.btnReg = findViewById(R.id.btnReg);
+    }
+    public void goReg(View view) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+    public void goLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
